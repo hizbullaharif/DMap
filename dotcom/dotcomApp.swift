6aade01c869001c50a084f85b5aaf21ad5f0ae52
hizb <hizbullahkhan887@gmail.com>
2025-09-02 16:55:47 +0500

    Initial Commit

---
+++ dotcom/dotcomApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotcomApp.swift
+//  dotcom
+//
+//  Created by arsal on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct dotcomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dotcomTests/dotcomTests.swift
@@ -0,0 +1,17 @@
+//
+//  dotcomTests.swift
+//  dotcomTests
+//
+//  Created by arsal on 9/2/25.
+//
+
+import Testing
+@testable import dotcom
+
+struct dotcomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
